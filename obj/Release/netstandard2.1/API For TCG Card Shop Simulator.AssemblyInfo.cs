475570d5fe08f782a30ef33589ec11bf3e167b48
Thin Creator 3483 <102002463+SaxbyMod@users.noreply.github.com>
2024-10-13 19:23:37 -0400

    Basic Patch for adding in New Cards (NDY)

--- obj/Release/netstandard2.1/API For TCG Card Shop Simulator.AssemblyInfo.cs
+++ obj/Release/netstandard2.1/API For TCG Card Shop Simulator.AssemblyInfo.cs
@@ -11,11 +11,17 @@
 using System;
 using System.Reflection;
 
+[assembly: System.Runtime.CompilerServices.IgnoresAccessChecksToAttribute("Assembly-CSharp")]
+[assembly: System.Runtime.CompilerServices.IgnoresAccessChecksToAttribute("Newtonsoft.Json")]
+[assembly: System.Runtime.CompilerServices.IgnoresAccessChecksToAttribute("Sirenix.Serialization")]
+[assembly: System.Runtime.CompilerServices.IgnoresAccessChecksToAttribute("Unity.TextMeshPro")]
+[assembly: System.Runtime.CompilerServices.IgnoresAccessChecksToAttribute("UnityEngine")]
+[assembly: System.Runtime.CompilerServices.IgnoresAccessChecksToAttribute("UnityEngine.UI")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("TheAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("The API For TCG Card Shop Simulator")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6b9fa603c88cc5c3217e30d2d50c4d2877b9c51")]
 [assembly: System.Reflection.AssemblyProductAttribute("TheAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TheAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
