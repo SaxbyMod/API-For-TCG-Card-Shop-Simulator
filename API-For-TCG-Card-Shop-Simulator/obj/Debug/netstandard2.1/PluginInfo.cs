439fd6ebc74471e7aec2e4c4da58d6e8489df648
Thin Creator 3483 <102002463+SaxbyMod@users.noreply.github.com>
2025-03-02 21:14:24 -0500

    Shifting things out as the Patcher doesnt need to be a subfolder anymore.

--- API-For-TCG-Card-Shop-Simulator/obj/Debug/netstandard2.1/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace API_For_TCG_Card_Shop_Simulator
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "TheAPI";
-        public const string PLUGIN_NAME = "TheAPI";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
